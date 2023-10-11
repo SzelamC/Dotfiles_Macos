f8e2cb1664012f6dacc7611fa3bc08ed90b7f453
SzelamC <chanszelam7725@gmail.com>
2023-10-11 13:03:29 +0800

    update

--- nvim/lua/szelc/plugins/treesitter.lua
+++ nvim/lua/szelc/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 local status, treesitter = pcall(require, "nvim-treesitter.configs")
 if not status then
-	return
+    return
 end
 
 vim.api.nvim_set_hl(0, "@rainbow.red", { fg = "#FD8A8A" })
@@ -10,50 +10,45 @@ vim.api.nvim_set_hl(0, "@rainbow.orange", { fg = "#FAAB7B" })
 
 -- configure treesitter
 treesitter.setup({
-	-- enable syntax highlighting
-	highlight = {
-		enable = true,
-		use_languagetree = true,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection = "gnn", -- set to `false` to disable one of the mappings
-			node_incremental = "grn",
-			scope_incremental = "grc",
-			node_decremental = "grm",
-		},
-	},
-	-- enable indentation
-	indent = { enable = true },
-	-- enable autotagging (w/ nvim-ts-autotag plugin)
-	autotag = { enable = true },
-	-- ensure these language parsers are installed
-	ensure_installed = {
-		"javascript",
-		"typescript",
-		"tsx",
-		"yaml",
-		"html",
-		"css",
-		"markdown",
-		"svelte",
-		"graphql",
-		"bash",
-		"lua",
-		"vim",
-		"dockerfile",
-		"gitignore",
-	},
+    -- enable syntax highlighting
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn", -- set to `false` to disable one of the mappings
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
+    -- enable indentation
+    indent = { enable = true },
+    -- enable autotagging (w/ nvim-ts-autotag plugin)
+    autotag = { enable = true },
+    -- ensure these language parsers are installed
+    ensure_installed = {
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "markdown",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+    },
 
-	context_commentstring = {
-		enable = true,
-	},
-	-- auto install above language parsers
-	auto_install = true,
+    context_commentstring = {
+        enable = true,
+    },
+    -- auto install above language parsers
+    auto_install = false,
 })
-
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
--- vim.treesitter.language.register("tsx", "typescript.tsx")
--- vim.treesitter.language.register("typescriptreact", "javascript")
