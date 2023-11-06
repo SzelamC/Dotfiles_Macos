2e3cec950366d1be93a86a079b06983e0dfc27d0
sze_lc <chanszelam7725@gmail.com>
2023-11-07 00:40:00 +0800

    update

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -26,6 +26,10 @@ return {
         },
         opts = function(_, opts)
             local cmp = require("cmp")
+            -- opts.preselect = cmp.PreselectMode.Item
+            opts.experimental.ghost_text = {
+                hl_group = "CmpItemKind",
+            }
             opts.mapping = vim.tbl_extend("force", opts.mapping, {
                 ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
                 ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
