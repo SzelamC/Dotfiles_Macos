3e8d06babcde1babf517189925962d98e7b6b128
SzelamC <chanszelam7725@gmail.com>
2023-11-07 13:26:21 +0800

    update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,21 +1,23 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-local augroup = vim.api.nvim_create_augroup
-local autocmds = vim.api.nvim_create_autocmd
 
-vim.cmd([[
-autocmd!
-au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=40})
-augroup END
-]])
+local function augroup(name)
+    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
 
-augroup("discontinue_comments", { clear = true })
-autocmds({ "FileType" }, {
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = augroup("highlight_yank"),
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 40 })
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "*" },
     callback = function()
         vim.opt.formatoptions = vim.opt.formatoptions - "o"
     end,
-    group = "discontinue_comments",
+    group = augroup("discontinue_comments"),
     desc = "Dont't continue comments with o/O",
 })
