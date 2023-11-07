092db64163ef55417be24916f3c4fdeefb648521
SzelamC <chanszelam7725@gmail.com>
2023-11-07 15:36:35 +0800

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -47,7 +47,7 @@ return {
                 eslint = function()
                     require("lazyvim.util").lsp.on_attach(function(client)
                         if client.name == "eslint" then
-                            client.server_capabilities.documentFormattingProvider = true
+                            client.server_capabilities.documentFormattingProvider = false
                         elseif client.name == "tsserver" then
                             client.server_capabilities.documentFormattingProvider = false
                         end
