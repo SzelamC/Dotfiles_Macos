60ae0705bb04f968c95be5d331c143ba71b8d9a4
sze_lc <chanszelam7725@gmail.com>
2023-11-11 20:15:29 +0800

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,9 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guicursor = ""
-vim.opt.updatetime = 50
 vim.opt.colorcolumn = "200"
-vim.opt.shiftwidth = 4
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.pumblend = 0
