09cf90cdac06fb4942ba057565073f8954343256
sze_lc <chanszelam7725@gmail.com>
2024-02-25 16:09:07 +0800

    update

--- nvim/lua/plugins/utils.lua
+++ nvim/lua/plugins/utils.lua
@@ -49,4 +49,18 @@ return {
     "gbprod/cutlass.nvim",
     config = true,
   },
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    opts = {
+      -- configuration goes here
+      arg = "leet",
+      lang = "golang",
+      injector = { ---@type table<lc.lang, lc.inject>
+        ["golang"] = {
+          before = { "package main" },
+        },
+      },
+    },
+  },
 }
