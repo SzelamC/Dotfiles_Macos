83f1007e89a52e6c7bc7e7ed0659b2b3df375eed
sam.slchan <sam.slchan@BASE.local>
2024-07-10 14:04:39 +0800

    update

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,4 +20,29 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-jdtls",
+    ft = { "java" },
+    opts = function(_, opts)
+      local lombok_path = "/Users/sam.slchan/.local/share/nvim/mason/packages/jdtls/lombok.jar"
+      opts.full_cmd = function(opts)
+        local fname = vim.api.nvim_buf_get_name(0)
+        local root_dir = opts.root_dir(fname)
+        local project_name = opts.project_name(root_dir)
+        local cmd = vim.deepcopy(opts.cmd)
+        vim.list_extend(cmd, {
+          "--jvm-arg=" .. string.format("-javaagent:%s", lombok_path),
+        })
+        if project_name then
+          vim.list_extend(cmd, {
+            "-configuration",
+            opts.jdtls_config_dir(project_name),
+            "-data",
+            opts.jdtls_workspace_dir(project_name),
+          })
+        end
+        return cmd
+      end
+    end,
+  },
 }
