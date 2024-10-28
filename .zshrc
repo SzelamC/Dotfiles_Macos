d5c325d1d362c3bb57de73a8be5d3e7a8aef3512
SzelamC <chanszelam7725@gmail.com>
2024-10-29 00:48:43 +0800

    update

--- .zshrc
+++ .zshrc
@@ -7,69 +7,25 @@ fi
 
 source ~/powerlevel10k/powerlevel10k.zsh-theme
 
+HISTFILE=$HOME/.zhistory
+SAVEHIST=1000
+HISTSIZE=999
+setopt share_history
+setopt hist_expire_dups_first
+setopt hist_ignore_dups
+setopt hist_verify
+bindkey "^[[A" history-search-backward
+bindkey "^[[B" history-search-forward
+
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # custom alias
-alias ls="exa --icons"
+alias ls="eza --icons"
 alias vim="nvim"
 alias g="git"
-alias cdg="cd ~/dev"
-alias cdc="cd ~/.config"
-alias cr="cargo run"
-alias crw="cargo watch -x run"
-alias pn=pnpm
-
-
-# pnpm
-export PNPM_HOME="/Users/szelc/Library/pnpm"
-case ":$PATH:" in
-  *":$PNPM_HOME:"*) ;;
-  *) export PATH="$PNPM_HOME:$PATH" ;;
-esac
-# pnpm end
-
-# wezterm config
-__vte_urlencode() (
-  # This is important to make sure string manipulation is handled
-  # byte-by-byte.
-  LC_ALL=C
-  str="$1"
-  while [ -n "$str" ]; do
-    safe="${str%%[!a-zA-Z0-9/:_\.\-\!\'\(\)~]*}"
-    printf "%s" "$safe"
-    str="${str#"$safe"}"
-    if [ -n "$str" ]; then
-      printf "%%%02X" "'$str"
-      str="${str#?}"
-    fi
-  done
-)
-
-__vte_osc7 () {
-  printf "\033]7;file://%s%s\007" "${HOSTNAME:-}" "$(__vte_urlencode "${PWD}")"
-}
-
-__vte_prompt_command() {
-  local command=$(HISTTIMEFORMAT= history 1 | sed 's/^ *[0-9]\+ *//')
-  command="${command//;/ }"
-  local pwd='~'
-  [ "$PWD" != "$HOME" ] && pwd=${PWD/#$HOME\//\~\/}
-  printf "\033]777;notify;Command completed;%s\007\033]0;%s@%s:%s\007%s" "${command}" "${USER}" "${HOSTNAME%%.*}" "${pwd}" "$(__vte_osc7)"
-}
-
-case "$TERM" in
-  xterm*|vte*)
-    [ -n "$BASH_VERSION" ] && PROMPT_COMMAND="__vte_prompt_command"
-    [ -n "$ZSH_VERSION"  ] && precmd_functions+=(__vte_osc7)
-    ;;
-esac
-
-true
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias cdg="z ~/dev"
+alias cdc="z ~/.config"
 
 # custom path
 export PATH=/Users/szelc/.local/bin:$PATH
@@ -84,16 +40,6 @@ export PATH=$PATH:$(go env GOPATH)/bin
 
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-source ~/.zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh
-bindkey '\t' menu-complete "$terminfo[kcbt]" reverse-menu-complete
-zstyle ':autocomplete:*complete*:*' insert-unambiguous yes
-zstyle ':autocomplete:*history*:*' insert-unambiguous yes
-zstyle ':autocomplete:menu-search:*' insert-unambiguous yes
 
-# bun completions
-[ -s "/opt/homebrew/Cellar/bun/1.0.0/share/zsh/site-functions/_bun" ] && source "/opt/homebrew/Cellar/bun/1.0.0/share/zsh/site-functions/_bun"
-
-[ -f "/Users/szelc/.ghcup/env" ] && source "/Users/szelc/.ghcup/env" # ghcup-env
-
-# bun completions
-[ -s "/Users/szelc/.bun/_bun" ] && source "/Users/szelc/.bun/_bun"
+source ~/powerlevel10k/powerlevel10k.zsh-theme
+eval "$(zoxide init zsh)"
---
+++ nix/flake.nix
@@ -0,0 +1,107 @@
+{
+  description = "Example Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, ... }: {
+      nixpkgs.config.allowUnfree = true;
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ 
+            pkgs.neovim
+            pkgs.aerospace
+            pkgs.wezterm
+            pkgs.raycast
+            pkgs.mas
+            pkgs.git
+            pkgs.zoxide
+            pkgs.eza
+            pkgs.go
+            pkgs.rustup
+            pkgs.nodejs
+            pkgs.fzf
+        ];
+
+    homebrew = {
+      	enable = true;
+        masApps = {
+        "Klack" = 6446206067;
+        "Hidden Bar" = 1452453066;
+
+        };
+        onActivation = {
+        cleanup = "zap";
+        };
+    };
+
+    fonts.packages = [
+        (pkgs.nerdfonts.override { fonts = [ "Iosevka" ]; })
+    ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      system.defaults = {
+	dock.autohide = true;
+	loginwindow.GuestEnabled = false;
+	NSGlobalDomain.AppleICUForce24HourTime = true;
+	NSGlobalDomain.AppleInterfaceStyle = "Dark";
+	NSGlobalDomain.KeyRepeat = 2;
+      };
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."szelam" = nix-darwin.lib.darwinSystem {
+      modules = [
+	configuration 
+	nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "szelam";
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."szelam".pkgs;
+  };
+}
