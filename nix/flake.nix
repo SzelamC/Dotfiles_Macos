9b0833cfe50ac670ff3e329137153ac6321994e3
sam.slchan <chanszelam7725@gmail.com>
2025-04-15 23:12:36 +0800

    update

--- nix/flake.nix
+++ nix/flake.nix
@@ -50,6 +50,9 @@
             pkgs.postman
             # pkgs.bruno
             pkgs.tmux
+            pkgs.uv
+            pkgs.jq
+            pkgs.redis
             # pkgs.deno
         ];
 
@@ -63,6 +66,8 @@
             "docker"
             "docker-compose"
             "czg"
+            "influxdb"
+            "influxdb-cli"
         ];
         casks = [
             "ghostty"
@@ -81,6 +86,7 @@
       # Auto upgrade nix package and the daemon service.
       services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
+      services.redis.enable = true;
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
