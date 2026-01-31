e6af3c01c3b644ac8bb0cff061bfd75bfb778563
sam.slchan <chanszelam7725@gmail.com>
2026-01-31 17:26:29 +0800

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,18 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guicursor = ""
-vim.opt.colorcolumn = "200"
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.pumblend = 0
+vim.opt.swapfile = false
 vim.opt.listchars = {
   tab = "  ",
 }
 
 vim.opt.backupcopy = "yes"
-
 vim.g.autoformat = false
 
 -- LSP Server to use for Python.
--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,5 @@
 local colorscheme = "catppuccin"
-local transparent_background = true
+
 return {
   {
     "LazyVim/LazyVim",
@@ -9,40 +9,6 @@ return {
     },
   },
   {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    opts = function()
-      return {
-        --- @usage 'auto'|'main'|'moon'|'dawn'
-        variant = "moon",
-        --- @usage 'main'|'moon'|'dawn'
-        dark_variant = "moon",
-        disable_background = true,
-        disable_float_background = true,
-        disable_italics = true,
-        highlight_groups = {
-          -- Blend colours against the "base" background
-          CursorLine = { bg = "foam", blend = 10 },
-          StatusLine = { fg = "love", bg = "love" },
-          StatusLineNC = { fg = "subtle", bg = "surface" },
-          TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-          TelescopeTitle = { fg = "base", bg = "love" },
-          TelescopePromptTitle = { fg = "base", bg = "pine" },
-          TelescopePreviewTitle = { fg = "base", bg = "iris" },
-          TelescopePromptBorder = { fg = "highlight_high", bg = "none" },
-          TelescopeBorder = { fg = "highlight_high", bg = "none" },
-          TelescopeNormal = { bg = "none" },
-          TelescopePromptNormal = { bg = "none" },
-          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
-          TelescopeSelection = { fg = "text", bg = "base" },
-          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
-          Comment = { italic = true },
-          ["@attribute"] = { fg = "iris" },
-        },
-      }
-    end,
-  },
-  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
@@ -50,14 +16,43 @@ return {
       transparent_background = true,
       float = {
         transparent = true, -- enable transparent floating windows
-        solid = true, -- use solid styling for floating windows, see |winborder|
       },
       no_italic = false,
       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
         conditionals = { "bold" },
       },
+      default_integrations = true,
       auto_integrations = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+      transparent = true,
+      styles = {
+        keywords = { italic = false },
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      dim_inactive = true,
+      lualine_bold = true,
+      plugins = {
+        all = false,
+        auto = false,
+        blink = true,
+        bufferline = true,
+        flash = true,
+        fzf = true,
+        gitsigns = true,
+        indent_blankline = true,
+        lazy = true,
+        snacks = true,
+        which_key = true,
+      },
     },
   },
 }
