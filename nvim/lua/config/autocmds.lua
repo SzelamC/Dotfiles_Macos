6f6e583aea55f35628b3a70771ea2ca17d07fbdd
sam.slchan <chanszelam7725@gmail.com>
2026-02-04 21:56:18 +0800

    update

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -10,3 +10,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
